3b78297cc64e492d2196c9ad9705535e0ef357d3
Tefik Rudari <tefikrudari99@gmail.com>
2020-07-13 17:06:48 +0200

    created search bar and home page shows available categories

--- models/Category.js
+++ models/Category.js
@@ -1,25 +1,23 @@
-    
-
-    module.exports = (sequelize, type) => { 
-    return sequelize.define(
+module.exports = (sequelize, type) => {
+  return sequelize.define(
     'Category',
     {
-      id:{
-          type: type.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false
+      id: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
       },
-      name:{
-          type: type.STRING,
-          allowNull: false
+      name: {
+        type: type.STRING,
+        allowNull: false
       },
       parentId: {
         type: type.INTEGER,
         references: {
           model: 'Categories',
           key: 'id'
-      }
+        }
       }
     }
   )
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="container m-9">
-    <section class="card-columns mt-5 items">
-      <!-- <div class="card karta">
-        <img class="card-img-top" src="https://source.unsplash.com/random/201x200" alt />
-        <div class="card-body">
-          <h4 class="card-title">Card Titile</h4>
-          <p class="card-text">This is the test of a short preview for a product description</p>
-          <a href class="btn btn-success btn-block">Read description</a>
+  <div class="container input-group m-9">
+    <div class="filter">
+      <input class="form-control" type="text" v-model="search" placeholder="Search Product" />
+      <Categories />
+    </div>
+    <section class="card-columns content mt-5">
+      <div class="card karta" v-for="product in filteredProducts" :key="product.id">
+        <div class="image">
+          <img class="card-img-top img-fluid" :src="getPath(product.image)" alt />
         </div>
-      </div> -->
-
-      <!-- **********     WHEN READY REPLACE WITH THE ONE BELOW    ********** -->
-
-      <div class="card karta" v-for="product in products" :key="product.id">
-        <img class="card-img-top" :src="getPath(product.image)" alt />
         <div class="card-body">
           <h4 class="card-title">{{product.name}}</h4>
           <p class="card-text">{{product.price}}</p>
-          <router-link :to="{name: '/', params: { id: product._id }}" class="btn btn-success btn-block">Read description</router-link>
+          <router-link
+            :to="{to: '/', params: { id: product._id }}"
+            class="btn btn-success btn-block"
+          >Read description</router-link>
         </div>
       </div>
     </section>
@@ -26,45 +24,53 @@
 
 <script>
 import axios from "axios";
+import Categories from "./Categories";
 
 export default {
   name: "HomePage",
   data() {
     return {
-      products: []
+      products: [],
+      search: ""
     };
   },
+  components: {
+    Categories
+  },
   created() {
-    axios.get(`http://localhost:3000/emall/api/products`)
-    .then(response => {
+    axios.get(`http://localhost:3000/emall/api/products`).then(response => {
       this.products = response.data;
     });
   },
-  methods:{
-      getPath(picturepath){
-        if(picturepath == null) {
-          return
-        }
-        return require(`../assets/${picturepath}`)
-      }
+  computed: {
+    filteredProducts: function() {
+      return this.products.filter(product => {
+        return product.name.toLowerCase().match(this.search.toLowerCase());
+      });
     }
+  },
+  methods: {
+    getPath(picturepath) {
+      if (picturepath == null) {
+        return;
+      }
+      return require(`../assets/${picturepath}`);
+    }
+  }
 };
 </script>
 
 <style scoped>
-.wrapper {
-  display: grid;
-  grid-gap: 20px;
+.karta {
+  max-width: 250px;
 }
-.items {
-  display: grid;
-  grid-gap: 35px;
-  max-width: 200px;
+.image {
+  height: 200px;
+  display: table-cell;
+  vertical-align: middle;
 }
-/* .karta {
-  max-width: 100%;
-} */
-.items img {
+.filter{
+  display: flex;
   width: 100%;
 }
 </style>
